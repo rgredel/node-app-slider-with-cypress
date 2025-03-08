035fa9dc10fc69deda52137601e51b2253ec3d50
rgredel <rgredel@csod.com>
2025-03-08 12:57:40 +0100

    add hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,25 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if third slide contains "Paris"', function () {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.card-description').should('contain', 'Rome');
+    cy.get('.card-description').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.card-description').should('contain', 'United Kingdom');
+    cy.get('.card-description').should('contain', 'London');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.card-description').should('contain', 'Paris');
+    cy.get('.card-description').should('contain', 'France');
+  });
+});
